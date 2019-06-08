216e3ecea56ae24f3936a215fd72a6f8eb7d3027
juliohm1978 <jhm@juliohm.com.br>
2019-06-08 19:16:18 -0300

    test

--- test/test.py
+++ test/test.py
@@ -5,9 +5,7 @@ import time
 logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] [%(levelname)-8s] [%(name)s] [%(funcName)s] %(message)s')
 
 c = k8scontroller.Controller('kool.karmalabs.local', 'guitars', kubeconfig_file='/home/lamento/.kube/kind-config-kind')
-c.set_api_timeout(5)
+c.set_api_timeout(0)
 th = c.watch_background()
-
-time.sleep(10)
 
 th.join()
