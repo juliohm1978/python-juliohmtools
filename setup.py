d48313c7985e011871ad2162349df94790d2a403
juliohm1978 <jhm@juliohm.com.br>
2019-06-08 19:23:59 -0300

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="juliohmtools",
-    version="0.0.11",
+    version="0.0.12",
     author="Julio H Morimoto",
     author_email="jhm@juliohm.com.br",
     description="A Collection of Python Libraries",
